e6a10941a2bff8316adb50a39df3b771644a63be
saurabh08687 <ssygi0007@gmail.com>
2020-06-03 00:51:03 +0530

    added postgresql as database

--- iLearn/settings.py
+++ iLearn/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'iLearn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iLearn',
+        'USER':'postgres',
+        'PASSWORD':'Love4Coding',
+        'HOST':'localHost'
     }
 }
 
