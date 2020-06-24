2c4b4151010092c15b1909709ef61225be18c091
Adarsh <adarshtheking28@gmail.com>
2020-06-24 09:07:56 -0400

    add backend categories

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,7 +1,13 @@
 from home.models import Courses
 
 def add_variable_to_context(request):
-    get_courses=Courses.objects.all()
-    get_courses={ 'get-courses':get_courses}
-    return get_courses
+    get_courses=list(Courses.objects.values())
+    courses=set()
+    sub_cat=set()
+    for course in get_courses:
+        courses.add(course['category'])
+        sub_cat.add((course['sub_category'],course['category']))
+    sorted(courses)
+    print(courses)
+    return { 'get_courses':courses,'sub_cat':sub_cat}
     
