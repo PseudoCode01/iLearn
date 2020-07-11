5b59e6d08c96646552f4e6251102afe1905dc7d1
saurabh08687 <ssygi0007@gmail.com>
2020-07-12 03:15:27 +0530

    added responsiveness to previewCourse Page

--- static/js/base.js
+++ static/js/base.js
@@ -55,15 +55,10 @@ window.onclick = function(event) {
   }
 }
 }
-//closeing the alert
-if(document.querySelector('.closeAlert')!==null)
-document.querySelector('.closeAlert').addEventListener('click',function(){
-  document.querySelector('.alert').style.display='none'
-})
-
 //open side nav
 /* Set the width of the side navigation to 250px */
 function openNav() {
+  console.log('clicked')
   document.getElementById("mySidenav").style.width = "250px";
 }
 
@@ -185,3 +180,23 @@ document.querySelector('.userIcon').addEventListener('mouseleave',function(){
   document.querySelector('.userData').style.display='none'
   document.querySelector('#cart').style.display='block'
 })
+
+// Get all elements with class="closebtn"
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+// Loop through all close buttons
+for (i = 0; i < close.length; i++) {
+  // When someone clicks on a close button
+  close[i].onclick = function(){
+
+    // Get the parent of <span class="closebtn"> (<div class="alert">)
+    var div = this.parentElement;
+
+    // Set the opacity of div to 0 (transparent)
+    div.style.opacity = "0";
+
+    // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
--- static/js/home.js
+++ static/js/home.js
@@ -2,14 +2,14 @@
 if(document.getElementById('mySidenav').style.width != 0){
     document.querySelector('.searchForm').style.display='none'
 }
-document.querySelector('.dropbtn').addEventListener('click',function(){
-    console.log('ff')
-    document.querySelector('.searchSection').style.display='none'
-})
-document.querySelector('.closebtn').addEventListener('click',function(){
-    console.log('ff')
-    document.querySelector('.searchSection').style.display='block'
-})
+// document.querySelector('.dropbtn').addEventListener('click',function(){
+//     console.log('ff')
+//     document.querySelector('.searchSection').style.display='none'
+// })
+// document.querySelector('.closebtn').addEventListener('click',function(){
+//     console.log('ff')
+//     document.querySelector('.searchSection').style.display='block'
+// })
 console.log(document.getElementById('home').offsetHeight)
 let flag = false;
 window.addEventListener('scroll', function(e) {
@@ -23,6 +23,7 @@ window.addEventListener('scroll', function(e) {
       document.querySelector('.logo').style.color='black'
       document.querySelector('.dropbtn').style.color='black'
       document.querySelector('.role').style.color='black'
+      document.querySelector('.myCourses').style.color='black'
     }
     
             if(scrollTop === 0){
@@ -30,6 +31,7 @@ window.addEventListener('scroll', function(e) {
       document.getElementById('myTopnav').style.boxShadow='0 0 0 0 black'
       document.querySelector('.logo').style.color='white'
       document.querySelector('.dropbtn').style.color='white'
+      document.querySelector('.myCourses').style.color='white'
       let role=document.querySelector('.role');
      role.style.color='white'
       role.addEventListener('mouseover',function(){
