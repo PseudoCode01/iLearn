1da668e2fc4ce9ede4c548842432713d4535b14a
Adarsh <adarshtheking28@gmail.com>
2020-07-29 04:09:35 -0400

    remove

--- iLearn/settings.py
+++ iLearn/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = 'cognedu/static/'
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
     
